1d7c755dea59bcfec704fe1bcbaed313860b620e
Mushishy <you@example.com>
2026-03-01 10:38:18 -0700

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+        // Consult https://svelte.dev/docs/kit/integrations
+        // for more information about preprocessors
+        preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			// Node.js adapter configuration
-			out: 'build',
-			precompress: false,
-			envPrefix: ''
-		}),
-		// Configure CSRF to allow requests from your application
-		csrf: {
-			trustedOrigins: ['http://100.67.101.72:3000', 'http://localhost:3000', 'https://localhost:3000']
-		}
-	}
+        kit: {
+                adapter: adapter({
+                        // Node.js adapter configuration
+                        out: 'build',
+                        precompress: false,
+                        envPrefix: ''
+                }),
+                // Configure CSRF to allow requests from your application
+                csrf: {
+                        trustedOrigins: ['http://localhost', 'https://localhost']
+                }
+        }
 };
 
 export default config;
